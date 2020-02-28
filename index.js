4b553acdda79e449fad424416fae15e3c7adebce
Fiona Bor <fionabor@seattleacademy.org>
2020-02-28 08:54:15 -0800

    Add attendance server feature

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 1505
+const attendance = [];
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+	let query = req.query;
+	attendance.push(query.name)
+	console.log(attendance);
+	res.send(attendance)
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
